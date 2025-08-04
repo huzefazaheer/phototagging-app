61c0cd9f8fd6f4802906608ce47bdbf859f61ef0
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-08-04 16:47:50 +0500

    Added exit buttons

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Canvas from './components/canvas/canvas.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <Canvas />
+    <App />
   </StrictMode>,
 )
