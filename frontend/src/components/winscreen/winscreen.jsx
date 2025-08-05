b9dffe81f2690065217e211e1bd0f885ebd6974d
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-08-05 22:37:15 +0500

    Updated styling for game win screen

--- frontend/src/components/winscreen/winscreen.jsx
+++ frontend/src/components/winscreen/winscreen.jsx
@@ -1,19 +1,55 @@
 import { useContext } from 'react'
 import { gameHandlerContext } from '../../App'
+import styles from './winscreen.module.css'
 
 export default function WinScreen() {
   const { timeElapsed } = useContext(gameHandlerContext)
 
   return (
     <>
-      <h3>Game end, you won. </h3>
-      <p>Time taken: {timeElapsed.current} seconds</p>
-      <form action="/leaderboard">
-        <h3>Leaderboard</h3>
-        <label htmlFor="username">Enter your name</label>
-        <input type="text" id="username" name="username" />
-        <button type="submit">Join Leaderboard</button>
-      </form>
+      <div className={styles.winscreen}>
+        <div className={styles.left}>
+          <h1>You won. </h1>
+          <p>You have successfully found all the targets!</p>
+          <h3 className={styles.timetaken}>
+            Time taken: {timeElapsed.current} seconds
+          </h3>
+          <form action="/leaderboard">
+            <div className={styles.input}>
+              {' '}
+              <label htmlFor="username">Enter display name</label>
+              <input type="text" id="username" name="username" />
+            </div>
+            <button type="submit">Join Leaderboard</button>
+          </form>
+        </div>
+        <div className={styles.right}>
+          <h2>Leaderboard</h2>
+          <h4>Map: The Beach</h4>
+          <table>
+            <tr>
+              <th>Username</th>
+              <th>Time </th>
+            </tr>
+            <tr>
+              <td>Admin</td>
+              <td>1.02 s</td>
+            </tr>
+            <tr>
+              <td>Huzefa</td>
+              <td>0.02 s</td>
+            </tr>
+            <tr>
+              <td>Ibrahim</td>
+              <td>5.02 s</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      <div className={styles.buttonholder}>
+        <button>Play Again</button>
+        <button>Choose another map</button>
+      </div>
     </>
   )
 }
--- frontend/src/utls/gamestate.js
+++ frontend/src/utls/gamestate.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from 'react'
 
 export default function useGameState() {
-  const [gameFinished, setGameFinished] = useState(true)
+  const [gameFinished, setGameFinished] = useState(false)
   const timeElapsed = useRef(0)
   const [targets, setTargets] = useState([])
 
